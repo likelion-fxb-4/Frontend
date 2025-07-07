32cf5e5d428197b85aeafe5453a8019f6539648c
박태근 <taegeun1234@naver.com>
2025-07-07 18:30:12 +0900

    수정2

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -20,7 +20,7 @@ export default function LoginPage({ setUserInfo }) {
         setIsLoading(true);
 
         axios
-            .post("https://silversieon.store:8080/api/auths/login", {
+            .post("https://silversieon.store/api/auths/login", {
                 username: id,
                 password: pass,
             })
