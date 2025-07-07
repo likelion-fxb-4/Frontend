244c24645643a0ad65a4e4ebec7248c0b3e1000b
박태근 <taegeun1234@naver.com>
2025-07-07 19:34:56 +0900

    찐찐최종

--- src/api.js
+++ src/api.js
@@ -1,14 +1,12 @@
-// src/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://silversieon.store:8080', // 🛠️ 실제 API 도메인으로 교체
+  baseURL: 'https://silversieon.store',
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
-// 로그인한 사용자 이름을 헤더에 자동 포함
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   const username = localStorage.getItem('username');
